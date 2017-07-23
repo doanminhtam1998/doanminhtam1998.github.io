454fdf76cfa5998312d65337efd7b40adabd0582
doanminhtam1998 <doanminhtam@gmail.com>
2017-07-23 21:13:43 +0700

    Complete for admin

--- script/admin.js
+++ script/admin.js
@@ -1,12 +1,16 @@
-// $.noConflict();
-// jQuery(document).ready(function() {
-//     $(".run").animate({
-//         width: '100%'
-//     }, 5000, function() {
-//         $('.run').hide();
-//     });
+$.noConflict();
+jQuery(document).ready(function() {
+    jQuery(".run").animate({
+        width: '100%'
+    }, 5000, function() {
+        jQuery('.run').hide();
+    });
+
+    jQuery(".admin-inf").click(function(){
+    	jQuery(".toggle-menu").toggle();
+    });
+   
+    
 
 
-
-
-// });
+});
