d53aa2b8cd5899d7161da76fe573ebf1ba8153a6
doanminhtam1998 <doanminhtam101198@gmail.com>
2017-07-24 21:17:24 +0700

    Add all post

--- script/app-admin.js
+++ script/app-admin.js
@@ -1,2 +1,23 @@
 var myApp = angular.module('myApp', ['textAngular']);
 //angular.module('myApp', [require('angular-sanitize'), require('textAngular')]);
+scotchApp.config(function($routeProvider) {
+    $routeProvider
+
+    // route for the home page
+        .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'mainController'
+    })
+
+    // route for the about page
+    .when('/about', {
+        templateUrl: 'pages/about.html',
+        controller: 'aboutController'
+    })
+
+    // route for the contact page
+    .when('/contact', {
+        templateUrl: 'pages/contact.html',
+        controller: 'contactController'
+    });
+});
