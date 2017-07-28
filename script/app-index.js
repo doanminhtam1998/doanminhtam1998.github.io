bb3708cf826d4200b77f129d08810268b7d6e8c7
doanminhtam1998 <doanminhtam101198@gmail.com>
2017-07-28 21:19:38 +0700

    Add API

--- script/app-index.js
+++ script/app-index.js
@@ -59,12 +59,43 @@ scotchApp.controller('mainController', function($scope, $http) {
     // create a message to display in our view
     var root = "https://green-web-blog.herokuapp.com";
 
-   $scope.message = 'Everyone come and see how good I look!';
+    $scope.message = 'Everyone come and see how good I look!';
+    var apiGetCat = function() {
+        $http.get(root + "/api/categories")
+            .then(function(response) {
+                //
+                $scope.Categories = response.data;
+            });
 
-   $scope.login = function() {
+    };
+
+    var apiGetArt = function() {
+        $http.get(root + "/api/articles")
+            .then(function(response) {
+                //
+                $scope.Articles = response.data;
+            });
+
+    };
+
+    var init = function() {
+        apiGetCat();
+        apiGetArt();
+    };
+
+    init();
+
+
+
+
+
+    console.log($scope.Categories)
+
+
+    $scope.login = function() {
         console.log($scope.user);
 
-       //POST Login API below:
+        //POST Login API below:
         $http.post(root + '/api/users/auth', $scope.user)
             .success(function(response) {
                 var isSuccess = response.success;
@@ -77,5 +108,6 @@ scotchApp.controller('mainController', function($scope, $http) {
             }).error(function(data, status, headers, config) {
                 console.log(data, status, headers, config);
             });
-        }
-    });
+    }
+
+});
