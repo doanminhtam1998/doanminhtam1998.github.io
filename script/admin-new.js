4a9490147b4e05d618779de8cead72c84498b6c2
doanminhtam1998 <doanminhtam@gmail.com>
2017-07-31 16:24:34 +0700

    Add amin-new.css

--- script/admin-new.js
+++ script/admin-new.js
@@ -1,5 +1,16 @@
-$.noConflict();
+
 jQuery(document).ready(function() {
-    console.log(jQuery('.tags-appear').val());
-    jQuery.
+    // console.log(jQuery('.tags-appear').val());
+    // var x=jQuery('.tags-appear').val();
+    // jQuery(".cat-input").change(function() {
+    // 	console.log(jQuery('.cat-input').val());
+    // 	x.push(jQuery('.cat-input').val());
+    	
+    // });
+     
+     jQuery(".cat-input").change(function() {
+    	
+
+    	jQuery('.tags-appear').tagsinput('add', jQuery('.cat-input').val(), {preventPost: true});
+     });
 });
