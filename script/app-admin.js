af11986d44f0398c1296fda17dee87e4e3ece718
doanminhtam1998 <doanminhtam101198@gmail.com>
2017-07-31 21:22:40 +0700

    Add admin-cat

--- script/app-admin.js
+++ script/app-admin.js
@@ -1,1 +1,41 @@
-var myApp = angular.module('myApp', ['textAngular']);
+var app = angular.module('myApp', ['textAngular']);
+app.controller("myCtrl", function($scope, $http) {
+    var root = "https://green-web-blog.herokuapp.com";
+
+    $scope.apiGetCat = function() {
+        $http.get(root + "/api/categories")
+            .then(function(response) {
+                $scope.categories = response.data;
+            })
+    };
+
+    $scope.apiGetArt = function() {
+        $http.get(root + "/api/articles")
+            .then(function(response) {
+                //
+                $scope.Articles = response.data;
+            });
+
+    };
+
+    // $scope.getCatNameOFArt = function() {
+    //     if (!undefined = $scope.categories) {
+    //         for (i = 0; i < $scope.categories.length; i++) {
+    //             var cat = $scope.categories[i];
+    //             if (cat._id == id) {
+    //                 return cat.name
+    //             };
+    //         };
+    //     };
+
+    // };
+
+    $scope.submitCreatArt = function() {
+
+    };
+
+    // var init = function() {
+    //     apiGetCat();
+    // }
+    // init();
+});
