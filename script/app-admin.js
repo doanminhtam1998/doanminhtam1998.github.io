b16cc7101dba1b23359e267cf83534e50d397453
doanminhtam1998 <doanminhtam101198@gmail.com>
2017-08-02 21:20:13 +0700

    Add API more

--- script/app-admin.js
+++ script/app-admin.js
@@ -43,6 +43,67 @@ app.controller("myCtrl", function($scope, $http) {
 
     }
 
+
+
+
+    $scope.submitCreateCategory = function() {
+
+        if ($scope.newCategory.name.length > 0 &&
+            $scope.newCategory.description.length > 0) {
+            $http({
+                method: 'POST',
+                url: root + "/api/categories",
+                data: $scope.newCategory
+            }).then(function successCallback(response) {
+                // $scope.categories.push(response.date);
+                $scope.apiGetCat();
+                $scope.newCategory.name = "";
+                $scope.newCategory.description = "";
+
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        } else {
+            alert("Input invalid");
+        }
+
+    }
+
+    $scope.submitCreateArticle = function() {
+        console.log($scope.newArticle);
+        $scope.newArticle._author = "5981d84fb38ced0004f0c5df";
+        $http.post(root + '/api/articles/', $scope.newArticle)
+            .then(function successCallbak(response) {
+                alert("Thành công");
+                window.location.href = 'admin.html';
+            }, function errorCallback(response) {
+                console.log(data, status, headers, config);
+            });
+    };
+
+    $scope.login = function() {
+        //console.log($scope.user);
+
+        //POST Login API below:
+        $http.post(root + '/api/users/auth', $scope.user)
+            .success(function(response) {
+                var isSuccess = response.success;
+                if (isSuccess) {
+                    console.log(response);
+                } else {
+                    //Raise Error
+                    alert(response.message);
+                }
+            })
+            .error(function(data, status, headers, config) {
+                console.log(data, status, headers, config);
+            });
+
+    };
+
+
+
+
     // var init = function() {
     //     apiGetCat();
     // }
